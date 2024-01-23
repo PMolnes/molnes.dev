d225a64dd3c19fcc7d42b7713c5117cf0956b236
PMolnes <pettermolnes@icloud.com>
2024-01-23 21:20:28 +0100

    add: content to home page

---
+++ src/lib/components/NavList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	const navigationItems = ['PROJECTS', 'EXPERIENCE', 'EDUCATION'];
+</script>
+
+<nav class="hidden md:block">
+	<ul class="flex flex-col font-mono gap-2">
+		{#each navigationItems as item, index}
+			<li>
+				<a
+					class="flex items-center gap-2 no-underline text-sm tracking-widest text-gray-400 hover:text-white group"
+					href="#projects"
+				>
+					<span>0{index + 1}</span>
+					<span
+						class="inline-block w-6 h-0.5 group-hover:w-12 group-hover:bg-white transition-all bg-gray-400"
+					></span>
+					<span class="underline">{item}</span>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import sopraPortait from '$lib/assets/sopra-portrett.jpg';
+	import NavList from '$lib/components/NavList.svelte';
+</script>
+
+<main class="flex flex-col md:flex-row mx-4 my-8">
+	<div>
+		<h1 class="text-6xl font-extrabold mb-2">Petter Molnes</h1>
+		<h2 class="text-2xl font-bold mb-6">Software Engineer</h2>
+		<p class="max-w-80">
+			I am Full-Stack developer, pursuing a Masters in Informatics while working part-time for <a
+				href="https://solwr.com">Solwr</a
+			>.
+		</p>
+		<p class="max-w-80 mt-4">In my spare time I work on side-projects and enjoy bouldering.</p>
+		<NavList />
+		<div id="socials" class="flex items-center gap-4 mt-10">
+			<img class="grayscale w-24 rounded-full" src={sopraPortait} alt="Portrait" />
+			<a href="https://github.com/PMolnes" target="_blank">GitHub</a>
+			<a href="https://www.linkedin.com/in/petter-molnes-532596202/" target="_blank">LinkedIn</a>
+		</div>
+	</div>
+	<div>
+		<p>project 1</p>
+		<p>project 1</p>
+		<p>project 1</p>
+		<p>project 1</p>
+	</div>
+</main>
