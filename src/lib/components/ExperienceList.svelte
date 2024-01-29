d91a4a39ba091757246f919c06ee57e98c0ad53f
PMolnes <pettermolnes@icloud.com>
2024-01-29 10:27:07 +0100

    update: solwr experiences to span two cols

--- src/lib/components/ExperienceList.svelte
+++ src/lib/components/ExperienceList.svelte
@@ -13,17 +13,26 @@
 			title: 'Store Clerk'
 		}
 	];
+
+	function spanTwo(company: string) {
+		if (company == 'Solwr') {
+			return 'col-span-2';
+		}
+		return '';
+	}
 </script>
 
-<section>
-	<h2 class="font-mono mb-6 tracking-widest lg:sr-only">EXPERIENCE</h2>
-	<div class="lg:grid lg:grid-cols-2 lg:gap-2">
+<section class="mt-20">
+	<h1 class="font-mono mb-6 tracking-widest lg:sr-only">EXPERIENCE</h1>
+	<div class="lg:grid lg:grid-cols-2 lg:gap-4">
 		{#each experiences as experience}
 			<a
 				href={`/experience/${experience.company}`}
-				class="bg-zinc-800 block p-14 pr-20 mb-4 lg:mb-0 lg:group-hover:opacity-50 lg:hover:!opacity-100 lg:hover:scale-105 transition-all"
+				class="{spanTwo(
+					experience.company
+				)} bg-zinc-800 block p-14 pr-20 mb-4 lg:mb-0 lg:group-hover:opacity-50 lg:hover:!opacity-100 lg:hover:scale-105 transition-all"
 			>
-				<h3 class="text-xl font-bold">{experience.title}</h3>
+				<h2 class="text-xl font-bold">{experience.title}</h2>
 				<p class="mb-4">{experience.company}</p>
 			</a>
 		{/each}
